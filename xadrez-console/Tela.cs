3618969db459c75ccea4d9d63673f198468c6424
Adriano Pimenta <apimenta.usp@gmail.com>
2020-09-29 08:18:19 -0300

    Personalizando o metodo imprimirTabuleiro

--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -8,10 +8,17 @@ namespace xadrez_console {
         public static void imprimirTabuleiro(Tabuleiro tab) {
 
             for (int i = 0; i < tab.linhas; i++) {
-                Console.Write($"{tab.linhas - i} ");
+                if (tab.linhas < 10) {
+                    Console.Write(tab.linhas - i + " ");
+                } else if (tab.linhas - i < 10) {
+                    Console.Write(" " + (tab.linhas - i) + " ");
+                } else {
+                    Console.Write(tab.linhas - i + " ");
+                }
                 for (int j = 0; j < tab.colunas; j++) {
                     if (tab.peca(i, j) == null) {
-                        Console.Write("- ");
+                        imprimirTraco(i, j);
+                        Console.Write(" ");
                     } else {
                         imprimirPeca(tab.peca(i, j));
                         Console.Write(" ");
@@ -19,16 +26,36 @@ namespace xadrez_console {
                 }
                 Console.WriteLine();
             }
-            Console.WriteLine("  a b c d e f g h");
+            if (tab.linhas < 10) {
+                Console.Write("  ");
+            } else {
+                Console.Write("   ");
+            }
+            char letra = 'a';
+            for (int i = 0; i < tab.colunas; i++) {
+                Console.Write((char)(letra + i) + " ");
+            }
+            Console.WriteLine();
         }
 
         public static void imprimirPeca(Peca peca) {
-            if(peca.cor == Cor.Branco) {
+            if (peca.cor == Cor.Branco) {
                 Console.Write(peca);
             } else {
                 ConsoleColor corPadrao = Console.ForegroundColor;
                 Console.ForegroundColor = ConsoleColor.Yellow;
                 Console.Write(peca);
+                Console.ForegroundColor = corPadrao;
+            }
+        }
+
+        public static void imprimirTraco(int i, int j) {
+            if (i%2 == 0 && j%2 == 0 || i%2 == 1 && j%2 == 1) {
+                Console.Write("-");
+            } else {
+                ConsoleColor corPadrao = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write("-");
                 Console.ForegroundColor = corPadrao;
             }
         }
