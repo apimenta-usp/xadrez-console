0e245846fefa22ef11062789b8f9c7bf29d4f486
Adriano Pimenta <apimenta.usp@gmail.com>
2020-09-29 15:57:54 -0300

    Testes iniciais com movimentos possiveis das pecas

--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -9,24 +9,75 @@ namespace xadrez_console {
         public static void imprimirTabuleiro(Tabuleiro tab) {
 
             for (int i = 0; i < tab.linhas; i++) {
-                if (tab.linhas < 10) {
-                    Console.Write(tab.linhas - i + " ");
-                } else if (tab.linhas - i < 10) {
-                    Console.Write(" " + (tab.linhas - i) + " ");
-                } else {
-                    Console.Write(tab.linhas - i + " ");
-                }
+                imprimirNumero(tab, i);
                 for (int j = 0; j < tab.colunas; j++) {
-                    if (tab.peca(i, j) == null) {
-                        imprimirTraco(i, j);
-                        Console.Write(" ");
-                    } else {
-                        imprimirPeca(tab.peca(i, j));
-                        Console.Write(" ");
-                    }
+                    imprimirPeca(tab.peca(i, j), i, j);
                 }
                 Console.WriteLine();
             }
+            imprimirLetra(tab);
+        }
+
+        public static void imprimirTabuleiro(Tabuleiro tab, bool[,] posicoesPossiveis) {
+
+            ConsoleColor fundoOriginal = Console.BackgroundColor;
+            ConsoleColor fundoAlterado = ConsoleColor.DarkGray;
+
+            for (int i = 0; i < tab.linhas; i++) {
+                imprimirNumero(tab, i);
+                for (int j = 0; j < tab.colunas; j++) {
+                    if (posicoesPossiveis[i, j]) {
+                        Console.BackgroundColor = fundoAlterado;
+                    } else {
+                        Console.BackgroundColor = fundoOriginal;
+                    }
+                    imprimirPeca(tab.peca(i, j), i, j);
+                    Console.BackgroundColor = fundoOriginal;
+                }
+                Console.WriteLine();
+                Console.BackgroundColor = fundoOriginal;
+            }
+            imprimirLetra(tab);
+        }
+
+        public static PosicaoXadrez lerPosicaoXadrez() {
+            string s = Console.ReadLine();
+            char coluna = s[0];
+            int linha = int.Parse(s[1] + "");
+            /* Na linha acima, para o programa entender o Parse do char na posição 1, foi 
+               preciso acrescentar as aspas para fazer uma conversão explícita para string. */
+            return new PosicaoXadrez(coluna, linha);
+        }
+
+        public static void imprimirPeca(Peca peca, int i, int j) {
+            if (peca == null) {
+                imprimirTraco(i, j);
+                Console.Write(" ");
+            } else {
+                if (peca.cor == Cor.Branco) {
+                    Console.Write(peca);
+                } else {
+                    ConsoleColor corPadrao = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.Write(peca);
+                    Console.ForegroundColor = corPadrao;
+                }
+                Console.Write(" ");
+            }
+        }
+
+        public static void imprimirTraco(int i, int j) {
+            if (i % 2 == 0 && j % 2 == 0 || i % 2 == 1 && j % 2 == 1) {
+                Console.Write("-");
+            } else {
+                ConsoleColor corPadrao = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write("-");
+                Console.ForegroundColor = corPadrao;
+            }
+        }
+
+        public static void imprimirLetra(Tabuleiro tab) {
             if (tab.linhas < 10) {
                 Console.Write("  ");
             } else {
@@ -39,34 +90,13 @@ namespace xadrez_console {
             Console.WriteLine();
         }
 
-        public static PosicaoXadrez lerPosicaoXadrez() {
-            string s = Console.ReadLine();
-            char coluna = s[0];
-            int linha = int.Parse(s[1] + "");
-            /* Na linha acima, para o programa entender o Parse do char na posição 1, foi 
-               preciso acrescentar as aspas para fazer uma conversão explícita para string. */
-            return new PosicaoXadrez(coluna, linha);
-        }
-
-        public static void imprimirPeca(Peca peca) {
-            if (peca.cor == Cor.Branco) {
-                Console.Write(peca);
+        public static void imprimirNumero(Tabuleiro tab, int i) {
+            if (tab.linhas < 10) {
+                Console.Write(tab.linhas - i + " ");
+            } else if (tab.linhas - i < 10) {
+                Console.Write(" " + (tab.linhas - i) + " ");
             } else {
-                ConsoleColor corPadrao = Console.ForegroundColor;
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.Write(peca);
-                Console.ForegroundColor = corPadrao;
-            }
-        }
-
-        public static void imprimirTraco(int i, int j) {
-            if (i%2 == 0 && j%2 == 0 || i%2 == 1 && j%2 == 1) {
-                Console.Write("-");
-            } else {
-                ConsoleColor corPadrao = Console.ForegroundColor;
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.Write("-");
-                Console.ForegroundColor = corPadrao;
+                Console.Write(tab.linhas - i + " ");
             }
         }
     }
--- xadrez-console/xadrez/Rei.cs
+++ xadrez-console/xadrez/Rei.cs
@@ -9,5 +9,68 @@ namespace xadrez {
         public override string ToString() {
             return "R";
         }
+
+        private bool podeMover(Posicao pos) {
+            Peca p = tab.peca(pos);
+            return p == null || p.cor != cor;
+        }
+
+        public override bool[,] movimentosPossiveis() {
+            bool[,] mat = new bool[tab.linhas, tab.colunas];
+            /* Vale lembrar que uma variável do tipo booleana recebe, 
+               na sua criação, por padrão, o valor false. */
+
+            Posicao pos = new Posicao(0, 0);
+
+            // Testando casa acima
+            pos.definirValores(posicao.linha - 1, posicao.coluna);
+            if(tab.posicaoValida(pos) && podeMover(pos)) {
+                mat[pos.linha, pos.coluna] = true;
+            }
+
+            // Testando casa nordeste
+            pos.definirValores(posicao.linha - 1, posicao.coluna + 1);
+            if (tab.posicaoValida(pos) && podeMover(pos)) {
+                mat[pos.linha, pos.coluna] = true;
+            }
+
+            // Testando casa direita
+            pos.definirValores(posicao.linha, posicao.coluna + 1);
+            if (tab.posicaoValida(pos) && podeMover(pos)) {
+                mat[pos.linha, pos.coluna] = true;
+            }
+
+            // Testando casa sudeste
+            pos.definirValores(posicao.linha + 1, posicao.coluna + 1);
+            if (tab.posicaoValida(pos) && podeMover(pos)) {
+                mat[pos.linha, pos.coluna] = true;
+            }
+
+            // Testando casa abaixo
+            pos.definirValores(posicao.linha + 1, posicao.coluna);
+            if (tab.posicaoValida(pos) && podeMover(pos)) {
+                mat[pos.linha, pos.coluna] = true;
+            }
+
+            // Testando casa sudoeste
+            pos.definirValores(posicao.linha + 1, posicao.coluna - 1);
+            if (tab.posicaoValida(pos) && podeMover(pos)) {
+                mat[pos.linha, pos.coluna] = true;
+            }
+
+            // Testando casa esquerda
+            pos.definirValores(posicao.linha, posicao.coluna - 1);
+            if (tab.posicaoValida(pos) && podeMover(pos)) {
+                mat[pos.linha, pos.coluna] = true;
+            }
+
+            // Testando casa noroeste
+            pos.definirValores(posicao.linha - 1, posicao.coluna - 1);
+            if (tab.posicaoValida(pos) && podeMover(pos)) {
+                mat[pos.linha, pos.coluna] = true;
+            }
+
+            return mat;
+        }
     }
 }
